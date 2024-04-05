1ee4f5068642017f75305eebca5627196aefbc02
Mathias <email@example.com>
2024-04-05 15:19:22 +0200

    save

---
+++ src/inline_json_main.rs
@@ -0,0 +1,18 @@
+// "Import" anything public in the parser module
+pub mod parser;
+
+fn main() {
+    // println!("Hello, world!");
+
+    // Parse the JSON
+    let result = parser::untyped_example();
+
+    // Handle errors from the parser if any
+    match result {
+        Ok(_result) => (),
+        Err(error) => print!("{}", error),
+    }
+}
+
+// cargo run 
+// cargo run inline_json_main
